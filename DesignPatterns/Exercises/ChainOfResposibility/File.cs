700b190133f53b4a127eb4f48a06bde5f94156e6
William Sanchez <wsanchez@obsglobal.com>
2022-03-18 15:12:48 -0500

    ChainOfResposibilityPattern

---
+++ DesignPatterns/Exercises/ChainOfResposibility/File.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Exercises.ChainOfResposibility
+{
+    public class File
+    {
+        public string FileName { get; set; }
+    }
+}
---
+++ DesignPatterns/Exercises/ChainOfResposibility/NumbersFormat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Exercises.ChainOfResposibility
+{
+    public class NumbersFormat : Handler
+    {
+        public NumbersFormat(Handler next) : base(next)
+        {
+        }
+
+        public override bool DoHandle(File file)
+        {
+            Console.WriteLine("Numbers Format.");
+            return false;
+        }
+
+        public override string GetExtension()
+        {
+            return ".numbers";
+        }
+    }
+}
