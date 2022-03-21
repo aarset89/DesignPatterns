962f6e5b0c48ff2c99f19839eb7c2a81950520e3
William Sanchez <wsanchez@obsglobal.com>
2022-03-21 13:50:50 -0500

    Composite pattern

---
+++ DesignPatterns/Exercises/Composite/Human.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Exercises.Composite
+{
+    public class Human : IComponent
+    {
+        public void GoToEmergency()
+        {
+            Console.WriteLine("Humman going to Emergency");
+        }
+    }
+}
---
+++ DesignPatterns/Exercises/Composite/Truck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Exercises.Composite
+{
+    public class Truck : IComponent
+    {
+        public void GoToEmergency()
+        {
+            Console.WriteLine("Truck going to Emergency");
+        }
+    }
+}
